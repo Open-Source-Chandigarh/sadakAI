159316040b27eefbb6dfe647719f74e7239c135b
bakayu <ayu.0067abc@gmail.com>
2024-12-01 20:02:56 +0530

    changed model to gemma2-9b-it

--- app/chat/config.py
+++ app/chat/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    MODEL_NAME = "llama3-70b-8192"
+    MODEL_NAME = "gemma2-9b-it"
     DEBUG = os.getenv("DEBUG", "False").lower() in ['true', '1', 't']
     GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
